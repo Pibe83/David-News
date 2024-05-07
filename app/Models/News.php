1ee1b3a24b2b849ec913960b1dd32b08c195fc4c
Pibe83 <davidscattone10@gmail.com>
2024-05-07 09:11:23 +0200

    First commit

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Traits\NewsSlugTrait;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Traits\NewsMutatorsTrait;
+use App\Models\Traits\NewsAccessorsTrait;
+use App\Models\Traits\NewsRelationshipTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class News extends Model
 {
-    use HasFactory;
+    use HasFactory,
+
+        NewsMutatorsTrait,
+        NewsAccessorsTrait,
+        NewsRelationshipTrait,
+        NewsSlugTrait;
 
     protected $fillable = [
         'title',
