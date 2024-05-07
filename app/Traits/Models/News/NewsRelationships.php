7662ecd8b583fc4c62a85de7ca5fc88d9afac37a
Gabriele Sbaiz <gabriele.sbaiz@2601.it>
2024-05-07 11:27:46 +0200

    Add types

--- app/Traits/Models/News/NewsRelationships.php
+++ app/Traits/Models/News/NewsRelationships.php
@@ -4,15 +4,17 @@ namespace App\Traits\Models\News;
 
 use App\Models\User;
 use App\Models\Comment;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 trait NewsRelationships
 {
-    public function comments()
+    public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
     }
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
